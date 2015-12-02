07db06ed83861c01a8a781ca46ce2906bd8bf066
trebloc <ctse77@alum.berkeley.edu>
2015-12-01 19:32:16 -0800

    Finished Homework Assignment

--- main.js
+++ main.js
@@ -4,5 +4,41 @@ $(document).ready(function() {
   console.log('JS is loaded!'); // sanity check
 
   // your code here
+$.ajax({
+method:'GET',
+url:'https://super-crud.herokuapp.com/books',
+    success: function (data) {
+    	//console.log(data.books[0].image);
+    	data.books.forEach(function (element){
+    		$('#books-list').append("<li>" + element.title + "</li>");
+    		$('#books-list').append("<li>" + element.author + "</li>");
+    		$('#books-list').append("<li><img src='" + element.image + "'></li>");
+    		$('#books-list').append("<li>" + element.releaseDate + "</li><hr>");
+    	})
+    } 
+})  
 
+var newBook = {};
+
+$('input#submit').on('click', function (event) {
+    event.preventDefault();
+
+newBook.title = $("input#title").val();  
+newBook.author = $("input#author").val(); 
+newBook.image = $("input#image").val(); 
+newBook.release = $("input#release").val(); 
+
+
+$.ajax({
+method:'POST',
+url:'https://super-crud.herokuapp.com/books', 
+data: newBook
+		})
+	})
 });
+
+// //$.ajax({
+// 	method: "GET/POST/PUT/DELETE",
+// 	url: "/books, /books:id",
+// 	data: {...}
+// })
