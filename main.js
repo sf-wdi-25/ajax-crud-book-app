6a2fb2e911cb0140165e05b2adeacd6a266a3075
Alex Rao <YOUR_GITHUB_EMAIL_ADDRESS>
2015-12-01 20:58:14 -0800

    Got info to show up on page.

--- main.js
+++ main.js
@@ -2,7 +2,21 @@
 $(document).ready(function() {
 
   console.log('JS is loaded!'); // sanity check
+  var baseUrl = "https://super-crud.herokuapp.com/books/"
 
-  // your code here
+	$.ajax({
+  		method: 'GET',
+  		url: baseUrl,
+  		success: function (element) {
+    		var allBooks = element.books;
+    		allBooks.forEach(function(ele) {
+    			var title = ele.title;
+    			var author = ele.author;
+    			var image = "<img src=" + ele.image + ">"
+    			var releaseDate = ele.releaseDate;
+    			$('#books-list').append("<p>" + image + "<br>" + "<strong>Title:</strong> " + title + "<br>" + "<strong>Author:</strong> " + author + "<br>" +"<strong>Release Date:</strong> " + releaseDate + "<p>")
+    		})
+  		}
+	})
 
 });
