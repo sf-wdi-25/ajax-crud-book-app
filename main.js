72417f562d7957844c4a7ba3df2386ba75a99be9
bachtsui <dbtsui2@gmail.com>
2015-12-01 17:09:52 -0800

    Can get and print out title of first book

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ $(document).ready(function() {
 
   console.log('JS is loaded!'); // sanity check
 
+  var readBooksUrl= "https://super-crud.herokuapp.com/books";
+
+  $.ajax({
+  	method: "GET",
+  	url: "https://super-crud.herokuapp.com/books",
+  	success: function (data){
+  		console.log(data.books[0].title);
+  	},
+  	error: function(){
+  		console.log("Mistake was made.");
+  	}
+  });
   // your code here
 
 });
