bdfd598710426fa1d45aff92831aba03bfde10ba
isangieri <isabelleangieri@gmail.com>
2015-12-01 22:35:17 -0800

    misunderstood the assignment. thought we needed to create a search function for some reason. starting on the create function

---
+++ main_snafu.js
@@ -0,0 +1,63 @@
+// wait for DOM to load before running JS
+$(document).ready(function() {
+
+var getAndPostEndpoint = 'https://super-crud.herokuapp.com/books';
+
+		$.ajax({
+			
+			// https://super-crud.herokuapp.com/books
+			// GET and POST
+			method: 'GET',
+			url: getAndPostEndpoint,
+			success: function (data,index) {
+				data.books.forEach(function(element,index){
+          var title = element.title;
+          var author = element.author;
+          var releaseDate = element.releaseDate;
+          var image = element.image;
+          var showBook = $('#books-list').append('<hr><p>Title: ' + title + '</p>' +
+          '</br><p>Author: ' + author + '</p>' +
+          '</br><p>Release Date: ' + releaseDate + '</p>' +
+          '</br><img class="small" src="' + image + '">');
+
+					$("#search").click(function(event) {
+					console.log(title);
+					var getTitle = $("#title").val();
+					var getAuthor = $("#author").val();
+					var getReleaseDate = $("#releaseDate").val();
+					
+					if(getTitle === title ||
+						getAuthor === author ||
+						getReleaseDate === releaseDate) {
+							console.log(image + releaseDate + title + author);
+							$("#books-list").html("");
+							//clear all of the books in the #books-list html div
+
+							//append the object that has that title into the html
+							$('#books-list').append('<hr><p>Title: ' + title + '</p>' +
+			        '</br><p>Author: ' + author + '</p>' +
+			        '</br><p>Release Date: ' + releaseDate + '</p>' +
+			        '</br><img class="small" src="' + image + '">');
+					}
+	});
+						});
+			}
+	});
+
+ //  $.ajax({
+
+	// 	// https://super-crud.herokuapp.com/books/:id
+	// 	// READS,UPDATES,DELETES one book
+
+	// 	method: 'GET/POST/PUT/DELETE',
+	// 	url: weekly_quakes_endpoint,
+	// 	data: {},
+	// 	success: function (data,index) {
+	// 		data.features.forEach(function(element,index){
+	// 			var title = element.properties.title;
+	// 		});
+	// 	}
+	// });
+  // your code here
+
+});
