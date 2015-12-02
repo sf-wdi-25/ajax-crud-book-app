a40ad4b3b11f29d1207226065b3fe607e4537995
kranjbar <YOUR_GITHUB_EMAIL_ADDRESS>
2015-12-01 18:13:24 -0800

    Read a list of all the books

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ $(document).ready(function() {
 
   console.log('JS is loaded!'); // sanity check
 
-  // your code here
-
+  $.ajax({
+  	method: "GET",
+  	url: "https://super-crud.herokuapp.com/books",
+  	success: function (data) {
+  		data.books.forEach(function(element) {
+  			$("#books-list").append("<img src=" + element.image + ">");
+  			$("#books-list").append("<p>Title: " + element.title + "</p>");
+  			$("#books-list").append("<p>Author: " + element.author + "</p>");
+  			$("#books-list").append("<p>Released: " + element.releaseDate + "</p>");
+  		});
+  	}
+  });
 });
