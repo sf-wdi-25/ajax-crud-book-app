b0f897ba3992b3b50a25a9a608785f264c41c5d1
anonym0us3 <nathan.shayevich@gmail.com>
2015-12-01 20:23:23 -0800

    apologies for not committing or pushing sooner. I was in the zone, the Danger Zone! Book images & associated meta-data loading, input fields and submit button built (non-functioning yet)

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+var booksList = "https://super-crud.herokuapp.com/books";
+
 // wait for DOM to load before running JS
 $(document).ready(function() {
 
   console.log('Braaaaaaiiiiiiiins!'); // sanity check
 
   // your code here
-
+	$.ajax ( {
+			method: "GET",
+			url: booksList,
+			success: function(ninja) {
+				ninja.books.forEach(function (unicorn) {
+					var images = unicorn.image;
+					var titles = unicorn.title;
+					var authors = unicorn.author;
+					var released = unicorn.releaseDate;
+					$('#books-list').append("<p><img src = '" + images + "'>" + "<br\>" + "<strong>Title: </strong>" + titles + "<br\>" + "<strong>Author: </strong>"+ authors + "<br\>" + "<strong>Release Date: </strong>"+ released + "</p>");
+				});
+			}
+		});
 });
