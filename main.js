9b655b9cbb4c062ff647a3750128bdb8eb28c904
isangieri <isabelleangieri@gmail.com>
2015-12-01 21:37:19 -0800

    honing in on the .click method function and deciding where it needs to be in order to get the scope of the success .forEach function variables. I am trying to compare inputs with each book object to see if there are any matching variables. If there are matching variables, then the corresponding book will be appended to the html

--- main.js
+++ main.js
@@ -3,48 +3,45 @@ $(document).ready(function() {
 
 var getAndPostEndpoint = 'https://super-crud.herokuapp.com/books';
 
-  console.log('JS is loaded!'); // sanity check
-
-	$('#search').click(function(event) {
-			$('#title').html('');
-
-			var getTitle = $('#title').val();
-			var getAuthor = $('#author').val();
-			var getReleaseDate = $('#releaseDate').val();
-			var getImage = $('#image').val();
-
-			if(getTitle === element.title ||
-				getAuthor === element.author ||
-				getReleaseDate === element.releaseDate ||
-				getImage === element.image) {
-					$('#books-list').html('');
-					//clear all of the books in the #books-list html div
-
-					//append the object that has that title into the html
-					$('#books-list').append('<hr><p>Title: ' + title + '</p>' +
-		          '</br><p>Author: ' + author + '</p>' +
-		          '</br><p>Release Date: ' + releaseDate + '</p>' +
-		          '</br><img src="' + image + '">');
-			}
-	});
 		$.ajax({
 			
 			// https://super-crud.herokuapp.com/books
 			// GET and POST
-
 			method: 'GET',
 			url: getAndPostEndpoint,
 			success: function (data,index) {
 				data.books.forEach(function(element,index){
-		          var title = element.title;
-		          var author = element.author;
-		          var releaseDate = element.releaseDate;
-		          var image = element.image;
-		          console.log(element);
-		          $('#books-list').append('<hr><p>Title: ' + title + '</p>' +
-		          '</br><p>Author: ' + author + '</p>' +
-		          '</br><p>Release Date: ' + releaseDate + '</p>' +
-		          '</br><img src="' + image + '">');
+          var title = element.title;
+          var author = element.author;
+          var releaseDate = element.releaseDate;
+          var image = element.image;
+          $('#books-list').append('<hr><p>Title: ' + title + '</p>' +
+          '</br><p>Author: ' + author + '</p>' +
+          '</br><p>Release Date: ' + releaseDate + '</p>' +
+          '</br><img src="' + image + '">');
+
+					$('#search').click(function(event) {
+					$('#title').html('');
+
+					var getTitle = '"' + $('#title').val() + '"';
+					var getAuthor = '"' + $('#author').val() + '"';
+					var getReleaseDate = '"' + $('#releaseDate').val() + '"';
+					var getImage = '"' + $('#image').val() + '"';
+					
+					if(getTitle === element.title ||
+						getAuthor === element.author ||
+						getReleaseDate === element.releaseDate ||
+						getImage === element.image) {
+							$('#books-list').html('');
+							//clear all of the books in the #books-list html div
+
+							//append the object that has that title into the html
+							$('#books-list').append('<hr><p>Title: ' + title + '</p>' +
+						  '</br><p>Author: ' + author + '</p>' +
+						  '</br><p>Release Date: ' + releaseDate + '</p>' +
+						  '</br><img src="' + image + '">');
+					}
+	});
 						});
 			}
 	});
