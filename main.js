7b115e26b2f522f92506d026bb0eb4e86dbc0dcb
Breon Knight <Breonknight7@gmail.com>
2015-12-01 20:21:55 -0800

    setting up ajax functions

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 // wait for DOM to load before running JS
 $(document).ready(function() {
 
-  console.log('JS is loaded!'); // sanity check
+
+	console.log('JS is loaded!'); // sanity check
 
   // your code here
+  	var books_endpoint = "https://super-crud.herokuapp.com/books/";
+	$(input).keydown(function (event){
+		if(event.which===13){
+			var keyInput = $('input').val().replace(" " + "+");
+
+		}
+	})
+
 
 });
