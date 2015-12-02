0cda0cb89a6c238db07a05bfd11ec9ea7fae47a1
kehontas <kehontas@gmail.com>
2015-12-01 19:52:06 -0800

    all book items on the page

--- main.js
+++ main.js
@@ -5,4 +5,21 @@ $(document).ready(function() {
 
   // your code here
 
+
+	$.ajax({
+		method: 'GET',
+		url: 'https://super-crud.herokuapp.com/books',
+		success: function (data) {
+			console.log(data);
+			data.books.forEach (function(book){
+				$("#books-list").append("<p>" + book.title  +  "</p>");
+				$("#books-list").append("<p>" + book.author  +  "</p>");
+				$("#books-list").append("<p>" + book.image  +  "</p>");
+				$("#books-list").append("<p>" + book.releaseDate  +  "</p>");
+				$("#books-list").append("<p>" + book._id  +  "</p>");
+			});
+		}
+
+	});
+
 });
