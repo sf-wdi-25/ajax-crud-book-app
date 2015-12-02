0e2a5d52b6ee830eab182c8fcc3792ade3ef9563
Noah Wimmer <nwimmer123@yahoo.com>
2015-12-01 17:37:07 -0800

    GET working, bringing in images, title etc

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ $(document).ready(function() {
 
   console.log('JS is loaded!'); // sanity check
 
+$.ajax({
+	method: 'GET',
+	url: 'https://super-crud.herokuapp.com/books',
+		success: function (display) {
+            display.books.forEach(function(element){
+            $("#books-list").append("<img src ='" + element.image + "'>");
+            $("#books-list").append("<p>" + "Title: " + element.title + "</p>");
+            $("#books-list").append("<p>" + "Author: " + element.author + "</p>");
+            $("#books-list").append("<p>" + "Release Date: " + element.releaseDate + "</p>");
+            })
+          console.log(display.books);
+		}	
+	});
+
+ //  $.ajax({
+	// method: 'GET',
+	// url: 'https://super-crud.herokuapp.com/books',
+	// 	success: function (display) {
+
+ //            $("#books-list").append("<img src ='" + image + "'>");
+	// 	}	
+	// });
+
   // your code here
 
 });
