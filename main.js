54779981ab76bc7bbd2eb64789537aae7bfa6e60
Noah Wimmer <nwimmer123@yahoo.com>
2015-12-01 21:55:35 -0800

    improved layout, failed at POST, must sleep

--- main.js
+++ main.js
@@ -3,29 +3,42 @@ $(document).ready(function() {
 
   console.log('JS is loaded!'); // sanity check
 
-$.ajax({
-	method: 'GET',
-	url: 'https://super-crud.herokuapp.com/books',
-		success: function (display) {
-            display.books.forEach(function(element){
-            $("#books-list").append("<img src ='" + element.image + "'>");
-            $("#books-list").append("<p>" + "Title: " + element.title + "</p>");
-            $("#books-list").append("<p>" + "Author: " + element.author + "</p>");
-            $("#books-list").append("<p>" + "Release Date: " + element.releaseDate + "</p>");
-            })
-          console.log(display.books);
-		}	
+	$.ajax({
+		method: 'GET',
+		url: 'https://super-crud.herokuapp.com/books',
+			success: function (display) {
+	            display.books.forEach(function(element){
+	            $("#books-list").append("<img src ='" + element.image + "'>");
+	            $("#books-list").append("<p>" + "Title: " + element.title + "</p>");
+	            $("#books-list").append("<p>" + "Author: " + element.author + "</p>");
+	            $("#books-list").append("<p>" + "Release Date: " + element.releaseDate + "</p>");
+	            });
+
+			}
+
+
+
+
+	//WHAT AM I DOING HERE!!
+		// method: "POST",
+		// 	success: function (display) {
+
+		// 	}
+		// });
+
 	});
 
- //  $.ajax({
-	// method: 'GET',
-	// url: 'https://super-crud.herokuapp.com/books',
-	// 	success: function (display) {
-
- //            $("#books-list").append("<img src ='" + image + "'>");
-	// 	}	
-	// });
-
-  // your code here
-
 });
+
+
+// $.ajax({
+//     method: 'POST',
+//     url: 'https://super-crud.herokuapp.com/books',
+//     dataType: 'json',
+   
+//     contentType: 'application/json',
+//     data: JSON.stringify( { "title": $('#title').val(), "author": $('#author').val() } ),
+
+// });
+
+
